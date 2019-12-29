a5101ef67ce743b88b10b9c83ce76a105b19c0ad
GooV <klik.9696@mail.ru>
2019-12-30 01:16:22 +0500

    first connection

--- coin_bot.py
+++ coin_bot.py
@@ -1,3 +1,12 @@
+import hashlib
+import socks
+import os
+
+from telethon import TelegramClient
+
+
+def get_hash(text):
+    return hashlib.md5(text.encode('utf-8')).hexdigest()
 
 
 class CoinBot:
@@ -5,6 +14,28 @@ class CoinBot:
         self.chat_name = chat_name
         self.api_id = api_id
         self.api_hash = api_hash
+        _hash = get_hash(self.api_id + self.api_hash)
+
+        if not os.path.isdir('./sessions'):
+            os.mkdir('./sessions')
+
+        self.session = f'./sessions/{_hash}.session'
+        # http://free-proxy.cz/ru/proxylist/country/all/socks5/ping/all
+        # proxy = (socks.SOCKS5, "192.169.202.106", 33731)
+        proxy = (socks.SOCKS5, "5.133.197.203", 24382)
+        self.client = TelegramClient(
+            self.session,
+            self.api_id,
+            self.api_hash,
+            proxy=proxy
+        )
 
     async def run(self):
-        pass
+        print(f' ====== RUN {self.chat_name} bot ======')
+
+        print('run started')
+        await self.client.start()
+        print('started success')
+
+        for dlg in await self.client.get_dialogs():
+            print(dlg.name)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-
+import asyncio
 import argparse
 import os
 import json
+
+from coin_bot import CoinBot
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -9,14 +11,35 @@ parser.add_argument(
     type=str,
     default='./config.json'
 )
+
 args = parser.parse_args()
 config_file = args.config_file
 
 if not os.path.isfile(config_file):
     raise Exception(f'File "{config_file} not fount"')
 
+
 with open(args.config_file, 'r', encoding='utf-8') as f:
     config_data = json.load(f)
 
-print(config_data)
-# args.config_file.close()
+
+# creating bots from config
+bots = [
+    CoinBot(
+        cfg['chat_name'],
+        cfg['api_id'],
+        cfg['api_hash']
+    )
+    for cfg in config_data
+]
+
+
+async def main():
+    # runing all bots
+    await asyncio.gather(*[bot.run() for bot in bots])
+
+
+if __name__ == "__main__":
+    # runing main
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
